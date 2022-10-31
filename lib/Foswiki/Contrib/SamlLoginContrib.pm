41a65c9c555e6fc62bcf8b3c7ce4c65acd6291a0
Timothy Legge <timlegge@gmail.com>
2022-10-31 19:07:28 -0300

    Update release and version number

--- lib/Foswiki/Contrib/SamlLoginContrib.pm
+++ lib/Foswiki/Contrib/SamlLoginContrib.pm
@@ -11,8 +11,8 @@ package Foswiki::Contrib::SamlLoginContrib;
 use strict;
 use warnings;
 
-our $VERSION = '1.18';
-our $RELEASE = '29 Oct 2022';
+our $VERSION = '1.19';
+our $RELEASE = '31 Oct 2022';
 
 our $SHORTDESCRIPTION = 'Provides a LoginManager to authenticate via Saml';
 
